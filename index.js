4f3d132725ddff14ffc98e81dbdd797e28d024ba
Sajibulislam098 <sajibul.islam098@gmail.com>
2021-10-27 01:24:36 +0600

    updated

--- index.js
+++ index.js
@@ -67,6 +67,9 @@ run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Running Genius Server");
 });
+app.get('/hello',(req, res) => {
+res.send('hello Updated Here')
+})
 app.listen(port, () => {
   console.log("Running Genius Server onPOrt ", port);
 });
